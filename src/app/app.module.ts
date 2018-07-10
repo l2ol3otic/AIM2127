593e146b140eae08501243b8072cc851c293a3f3
l2ol3otic <l2ol3otic@gmail.com>
2018-07-10 23:01:52 +0700

    rebuild code SP4807

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppRouting } from './app.routing';
+import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule, FormsModule } from '../../node_modules/@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
